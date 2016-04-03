9a842e37185dd1b7afe0f367e92f7b48664ac4be
NOBUOKA Yu <nobuoka@vividcode.info>
2016-04-04 08:39:00 +0900

    コメント追加

---
+++ app/src/main/java/info/vividcode/android/sqr/presentation/models/package.java
@@ -0,0 +1,5 @@
+/**
+ * プレゼンテーション層のモデルのためのパッケージ。
+ * このパッケージ内のクラスは画面表示のためのためのものではあるが、View のことは何も知らないようにすること。
+ */
+package info.vividcode.android.sqr.presentation.models;
--- app/src/main/java/info/vividcode/android/sqr/presentation/package.java
+++ app/src/main/java/info/vividcode/android/sqr/presentation/package.java
@@ -1,4 +1,5 @@
 /**
- * プレゼンテーション層。
+ * プレゼンテーション層。 内部はさらに MVP パターンで記述する。
+ * プレゼンテーション層のモデルがアプリケーション層に依存する形になる。
  */
 package info.vividcode.android.sqr.presentation;
