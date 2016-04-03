462b39f18e3c361736d71d41542b1861e514bc9c
NOBUOKA Yu <nobuoka@vividcode.info>
2016-04-03 23:30:02 +0900

    コメント追加

--- app/src/main/kotlin/info/vividcode/android/sqr/dto/QiitaItem.kt
+++ app/src/main/kotlin/info/vividcode/android/sqr/dto/QiitaItem.kt
@@ -12,15 +12,24 @@ data class QiitaItem(
         @SerializedName("body")
         var body: String,
 
+        /** HTML でマークアップされた文字列。 */
         @SerializedName("rendered_body")
         var renderedBody: String,
 
         @SerializedName("coediting")
         var coediting: Boolean,
 
+        /**
+         * 『2000-01-01T00:00:00+00:00』 のような時刻を表す文字列。
+         * TODO : [com.google.gson.annotations.JsonAdapter] で変換する。
+         */
         @SerializedName("created_at")
         var createdAt: String,
 
+        /**
+         * 『2000-01-01T00:00:00+00:00』 のような時刻を表す文字列。
+         * TODO : [com.google.gson.annotations.JsonAdapter] で変換する。
+         */
         @SerializedName("updated_at")
         var updatedAt: String,
 
